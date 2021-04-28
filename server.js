180b55d39275fae2c9ee402e02ddc9de9c630488
rvnltf <aldirivan56@gmail.com>
2021-04-28 21:12:54 +0700

    Membuat HTTP Server

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("Hello Javascript!");
+const http = require('http');
+
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+
+    response.statusCode = 200;
+    response.end('<h1>Halo HTPP Server!</h1>')
+}
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+
+const host = 'localhost';
+
+server.listen(port, host, ()=> {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+})
