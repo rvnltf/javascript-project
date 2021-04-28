4823c69d92e858528cddf89498d3a1097701076a
rvnltf <aldirivan56@gmail.com>
2021-04-28 21:19:20 +0700

    Response Status

--- server.js
+++ server.js
@@ -3,20 +3,23 @@ const http = require('http');
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
 
-    response.statusCode = 200;
     
     const {method, url} = request;
 
     if (url === '/') {
         if(method === 'GET'){
+            response.statusCode = 200;
             response.end("Ini adalah homepage");
         } else {
+            response.statusCode = 400;
             response.end(`Halaman tidak dapat diakses dengan ${method} request`);
         }
     } else if (url === '/about') {
         if(method === 'GET'){
+            response.statusCode = 200;
             response.end("Halo! Ini adalah halaman about");
         } else if(method === 'POST'){
+            response.statusCode = 200;
             let body = [];
 
             request.on('data', chunk => {
@@ -29,9 +32,11 @@ const requestListener = (request, response) => {
                 response.end(`Halo, ${name}! Ini adalah halaman about`);
             })
         } else {
+            response.statusCode = 400;
             response.end(`Halaman tidak dapat diakses dengan ${method} request`);
         }
     } else {
+        response.statusCode = 404;
         response.end("Halaman tidak ditemukan!");
     }
 
