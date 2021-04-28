1f4d23b5f4655fbe70f1e63b710a7e094f8ef0bc
rvnltf <aldirivan56@gmail.com>
2021-04-28 21:13:42 +0700

    Method/Verb Request

--- server.js
+++ server.js
@@ -4,7 +4,21 @@ const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
 
     response.statusCode = 200;
-    response.end('<h1>Halo HTPP Server!</h1>')
+    
+    const {method} = request;
+
+    if(method === 'GET'){
+        response.end("Ini method GET!");
+    }
+    if(method === 'POST'){
+        response.end("Ini method POST!");
+    }
+    if(method === 'PUT'){
+        response.end("Ini method PUT!");
+    }
+    if(method === 'DELETE'){
+        response.end("Ini method DELETE!");
+    }
 }
 
 const server = http.createServer(requestListener);
